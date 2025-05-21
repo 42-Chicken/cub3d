8940de5d37216e9c2bfd7032d5f9a8e12d3742db
R0-main <justromixx@gmail.com>
2025-05-21 10:23:18 +0200

    fix minimap

--- src/game/player/player.c
+++ src/game/player/player.c
@@ -6,7 +6,7 @@
 /*   By: rguigneb <rguigneb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:27:34 by rguigneb          #+#    #+#             */
-/*   Updated: 2025/05/19 22:58:00 by rguigneb         ###   ########.fr       */
+/*   Updated: 2025/05/21 10:22:41 by rguigneb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,54 +20,46 @@ void	set_player_position_angle(t_cub3d *cub3d, t_dvec2 pos, double angle)
 
 // in your movement/collision file, replace can_move with this:
 
-static bool can_move(t_cub3d *cub3d, double x, double y, double r)
+static bool	can_move(t_cub3d *cub3d, double x, double y, double r)
 {
-    // compute the 4 corners of the player's bounding box
-    double left   = x - r;
-    double right  = x + r;
-    double top    = y - r;
-    double bottom = y + r;
+	double	left;
+	double	right;
+	double	top;
+	double	bottom;
 
-    // check each corner against the map
-    if (map_is_wall(cub3d,
-            (size_t)floor(left),
-            (size_t)floor(top)))
-        return false;
-    if (map_is_wall(cub3d,
-            (size_t)floor(right),
-            (size_t)floor(top)))
-        return false;
-    if (map_is_wall(cub3d,
-            (size_t)floor(right),
-            (size_t)floor(bottom)))
-        return false;
-    if (map_is_wall(cub3d,
-            (size_t)floor(left),
-            (size_t)floor(bottom)))
-        return false;
-
-    return true;
+	// compute the 4 corners of the player's bounding box
+	left = x - r;
+	right = x + r;
+	top = y - r;
+	bottom = y + r;
+	// check each corner against the map
+	if (map_is_wall(cub3d, (size_t)floor(left), (size_t)floor(top)))
+		return (false);
+	if (map_is_wall(cub3d, (size_t)floor(right), (size_t)floor(top)))
+		return (false);
+	if (map_is_wall(cub3d, (size_t)floor(right), (size_t)floor(bottom)))
+		return (false);
+	if (map_is_wall(cub3d, (size_t)floor(left), (size_t)floor(bottom)))
+		return (false);
+	return (true);
 }
 
 void	handle_player_movements(t_cub3d *cub3d)
 {
-	double		mult;
-	double		dx, dy;
+	double	mult;
+
+	double dx, dy;
 	mult = 0;
 	if (is_pressed(cub3d, 'w'))
-		mult +=  1;
+		mult += 1;
 	if (is_pressed(cub3d, 's'))
 		mult += -1;
 	dx = cub3d->player.cos_r * mult * (cub3d->settings.player_speed / 10.0);
 	dy = cub3d->player.sin_r * mult * (cub3d->settings.player_speed / 10.0);
-	if (can_move(cub3d,
-			cub3d->player.location.x + dx,
-			cub3d->player.location.y,
+	if (can_move(cub3d, cub3d->player.location.x + dx, cub3d->player.location.y,
 			PLAYER_COLLISION_RADIUS))
 		cub3d->player.location.x += dx;
-	if (can_move(cub3d,
-			cub3d->player.location.x,
-			cub3d->player.location.y + dy,
+	if (can_move(cub3d, cub3d->player.location.x, cub3d->player.location.y + dy,
 			PLAYER_COLLISION_RADIUS))
 		cub3d->player.location.y += dy;
 }
@@ -94,8 +86,6 @@ void	update_player(t_cub3d *cub3d)
 {
 	handle_player_movements(cub3d);
 	handle_player_rotation(cub3d);
-	cub3d->player.minimap_pos.x = cub3d->player.location.x * MINIMAP_TILE_SIZE
-		+ MINIMAP_TILE_SIZE / 2;
-	cub3d->player.minimap_pos.y = cub3d->player.location.y * MINIMAP_TILE_SIZE
-		+ MINIMAP_TILE_SIZE / 2;
+	cub3d->player.minimap_pos.x = cub3d->player.location.x * MINIMAP_TILE_SIZE;
+	cub3d->player.minimap_pos.y = cub3d->player.location.y * MINIMAP_TILE_SIZE;
 }
